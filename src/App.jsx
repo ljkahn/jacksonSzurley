74dbd50b0e154baa082b357ca0a47ccb6ce0f64f
Lia Kahn <liakahn@Lias-MBP.hsd1.co.comcast.net>
2024-01-25 15:11:00 -0700

    added photos into material UI masonry layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
 import { useState } from "react";
-
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Pictures from "./Pages/pictureDisplay";
+import Contact from "./Pages/Contact";
+import Store from "./Pages/Store";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Router>
+        <Header />
+
+        <Routes>
+          <Route path="/" element={<Pictures />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/store" element={<Store />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </>
+  );
 }
 
 export default App;
--- src/Pages/pictureDisplay.jsx
+++ src/Pages/pictureDisplay.jsx
@@ -2,10 +2,94 @@ import React from "react";
 import Box from "@mui/material/Box";
 import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
+import Arch from "../assets/workPhotos/arch.jpg";
+import Cabin from "../assets/workPhotos/cabin.jpg";
+import City from "../assets/workPhotos/city.jpg";
+import Cloudy from "../assets/workPhotos/cloudy.jpg";
+import Sully from "../assets/workPhotos/fatSullys.jpg";
+
+import Fog from "../assets/workPhotos/fog.jpg";
+import Glass from "../assets/workPhotos/glass.jpg";
+import Golden from "../assets/workPhotos/golden.jpg";
+import Grey from "../assets/workPhotos/greyMtn.jpg";
+import Oasis from "../assets/workPhotos/oasis.jpg";
+import Sand from "../assets/workPhotos/sand.jpg";
+
+import Sun from "../assets/workPhotos/sunCity.jpg";
+import Texture from "../assets/workPhotos/texture.jpg";
+import Tree from "../assets/workPhotos/tree.jpg";
+import Water from "../assets/workPhotos/water.jpg";
+import Mtn from "../assets/workPhotos/waterMtn.jpg";
 
 function pictureDisplay() {
+  const itemData = [
+    {
+      img: Arch,
+      title: "arch",
+    },
+    {
+      img: Cabin,
+      title: "Books",
+    },
+    {
+      img: City,
+      title: "Sink",
+    },
+    {
+      img: Cloudy,
+      title: "Kitchen",
+    },
+    {
+      img: Sully,
+      title: "Blinds",
+    },
+    {
+      img: Golden,
+      title: "Chairs",
+    },
+    {
+      img: Glass,
+      title: "Laptop",
+    },
+    {
+      img: Water,
+      title: "Doors",
+    },
+    {
+      img: Oasis,
+      title: "Coffee",
+    },
+    {
+      img: Fog,
+      title: "Storage",
+    },
+    {
+      img: Grey,
+      title: "Candle",
+    },
+    {
+      img: Sand,
+      title: "Coffee table",
+    },
+    {
+      img: Mtn,
+      title: "Coffee table",
+    },
+    {
+      img: Texture,
+      title: "Coffee table",
+    },
+    {
+      img: Tree,
+      title: "Coffee table",
+    },
+    {
+      img: Sun,
+      title: "Coffee table",
+    },
+  ];
   return (
-    <Box sx={{ width: 500, height: 450, overflowY: "scroll" }}>
+    <Box sx={{ width: 700, height: 450, overflowY: "scroll" }}>
       <ImageList variant="masonry" cols={3} gap={8}>
         {itemData.map((item) => (
           <ImageListItem key={item.img}>
@@ -21,56 +105,5 @@ function pictureDisplay() {
     </Box>
   );
 }
-
-const itemData = [
-  {
-    img: "https://images.unsplash.com/photo-1549388604-817d15aa0110",
-    title: "Bed",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1525097487452-6278ff080c31",
-    title: "Books",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1523413651479-597eb2da0ad6",
-    title: "Sink",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1563298723-dcfebaa392e3",
-    title: "Kitchen",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1588436706487-9d55d73a39e3",
-    title: "Blinds",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1574180045827-681f8a1a9622",
-    title: "Chairs",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1530731141654-5993c3016c77",
-    title: "Laptop",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1481277542470-605612bd2d61",
-    title: "Doors",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1517487881594-2787fef5ebf7",
-    title: "Coffee",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1516455207990-7a41ce80f7ee",
-    title: "Storage",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1597262975002-c5c3b14bbd62",
-    title: "Candle",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1519710164239-da123dc03ef4",
-    title: "Coffee table",
-  },
-];
 
 export default pictureDisplay;
